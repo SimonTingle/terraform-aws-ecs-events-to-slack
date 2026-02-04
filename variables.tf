d9ee68bfbaf1d4862d159bb2b153ffdb37b9f832
Simon Tingle <tingleteaching@gmail.com>
2026-02-04 15:55:47 +0000

    feat: modernize repo structure and implement hardened terraform
    - Migrated dependency management to uv
    - Added .kiro steering documentation and AGENTS.md
    - Implemented hardened IAM policies and OIDC-ready CI
    - Cleaned .gitignore and added dependabot

---
+++ variables.tf
@@ -0,0 +1,122 @@
+
+variable "name" {
+  description = "The string which will be used for the name of AWS Lambda function and other created resources"
+  type        = string
+}
+
+variable "slack_webhook_url" {
+  description = <<EOT
+  (default) A Slack incoming webhook URL. 
+  (if slack_webhook_url_source_type is 'secret') A secretsmanager secret name 
+  (if slack_webhook_url_source_type is 'ssm') The full path to the SSM parameter including the initial slash.
+  EOT
+  type        = string
+}
+
+
+
+variable "role_name" {
+  description = "The string which will be used for the name of Lambda IAM role"
+  type        = string
+  default     = null
+}
+
+variable "create_role" {
+  description = "Controls whether IAM role for Lambda Function should be created"
+  type        = bool
+  default     = true
+}
+
+variable "lambda_role" {
+  description = "IAM role ARN attached to the Lambda Function. This governs both who / what can invoke your Lambda Function, as well as what resources our Lambda Function has access to. See Lambda Permission Model for more details."
+  type        = string
+  default     = ""
+}
+
+variable "slack_webhook_url_source_type" {
+  description = "Define where to get the slack webhook URL for variable slack_webhook_url. Either as text input or from an AWS secretsmanager lookup"
+  validation {
+    condition     = contains(["text", "secretsmanager", "ssm"], var.slack_webhook_url_source_type)
+    error_message = "Invalid source type. Must be one of 'text', 'secretsmanager', 'ssm'."
+  }
+  type    = string
+  default = "text"
+}
+
+variable "enable_ecs_task_state_event_rule" {
+  description = "The boolean flag enabling the EvenBridge Rule for `ECS Task State Change` events. The `detail` section of this rule is configured with `ecs_task_state_event_rule_detail` variable."
+  type        = bool
+  default     = true
+}
+
+variable "enable_ecs_deployment_state_event_rule" {
+  description = "The boolean flag enabling the EvenBridge Rule for `ECS Deployment State Change` events. The `detail` section of this rule is configured with `ecs_deployment_state_event_rule_detail` variable."
+  type        = bool
+  default     = true
+}
+
+variable "enable_ecs_service_action_event_rule" {
+  description = "The boolean flag enabling the EvenBridge Rule for `ECS Service Action` events. The `detail` section of this rule is configured with `ecs_service_action_event_rule_detail` variable."
+  type        = bool
+  default     = true
+}
+
+variable "ecs_task_state_event_rule_detail" {
+  description = "The content of the `detail` section in the EvenBridge Rule for `ECS Task State Change` events. Use it to filter the events which will be processed and sent to Slack."
+  type        = any
+  default = {
+    lastStatus    = ["STOPPED"]
+    stoppedReason = [{ "anything-but" : { "prefix" : "Scaling activity initiated by (deployment ecs-svc/" } }]
+  }
+}
+
+variable "ecs_deployment_state_event_rule_detail" {
+  description = "The content of the `detail` section in the EvenBridge Rule for `ECS Deployment State Change` events. Use it to filter the events which will be processed and sent to Slack."
+  type        = any
+  default = {
+    eventType = ["ERROR"]
+  }
+}
+
+variable "ecs_service_action_event_rule_detail" {
+  description = "The content of the `detail` section in the EvenBridge Rule for `ECS Service Action` events. Use it to filter the events which will be processed and sent to Slack."
+  type        = any
+  default = {
+    eventType = ["WARN", "ERROR"]
+  }
+}
+
+variable "custom_event_rules" {
+  description = "A map of objects representing the custom EventBridge rule which will be created in addition to the default rules."
+  type        = any
+  default     = {}
+
+  validation {
+    error_message = "Each rule object should have both 'detail' and 'detail-type' keys."
+    condition     = alltrue([for name, rule in var.custom_event_rules : length(setintersection(keys(rule), ["detail", "detail-type"])) == 2])
+  }
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "cloudwatch_logs_retention_in_days" {
+  description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653."
+  type        = number
+  default     = 14
+}
+
+variable "lambda_memory_size" {
+  description = "Amount of memory in MB your Lambda Function can use at runtime. Valid value between 128 MB to 10,240 MB (10 GB), in 64 MB increments."
+  type        = number
+  default     = 256
+}
+
+variable "recreate_missing_package" {
+  description = "Whether to recreate missing Lambda package"
+  type        = bool
+  default     = true
+}
